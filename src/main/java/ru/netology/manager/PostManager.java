1499a7e95bb75676b3d1fa92a7d784ec59e93645
Leiza111 <leiza-rad@yandx.ru>
2021-11-23 21:07:56 +0300

    V2

--- src/main/java/ru/netology/manager/PostManager.java
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -2,9 +2,13 @@ package ru.netology.manager;
 
 public class PostManager {
     private Post[] posts;
-    public Post[] getPostForFeed() {
-        // TODO: add logic
 
-        return null;
-    }
+        public Post[] search(int ownerID, String domain, String query, boolean ownersOnly, int count, int offset, boolean extended) {
+            return null;
+        }
+
+        public void delete(int ownerId, int postId) {
+
+        }
+
 }
