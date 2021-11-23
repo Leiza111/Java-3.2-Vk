1d8913dd7357d04dd53ba00da3abc143a5aba276
Leiza111 <leiza-rad@yandx.ru>
2021-11-23 20:57:08 +0300

    V2

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,10 @@
+package ru.netology.manager;
+
+public class PostManager {
+    private Post[] posts;
+    public Post[] getPostForFeed() {
+        // TODO: add logic
+
+        return null;
+    }
+}
