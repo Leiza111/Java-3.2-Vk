1d454377be9a810220f7f4d42adf13874af76dcf
Leiza111 <leiza-rad@yandx.ru>
2021-11-30 08:23:50 +0300

    modification

---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,22 @@
+package ru.netology.domain;
+
+public class Post {
+
+    private int id;
+    private String text;
+    private int data; // дата
+    private String heading; // заголовок
+    private String complain;
+
+    private Comment comment;
+    private Copyright copyright;
+    private Donut donut;
+    private Geo geo;
+    private Like like;
+    private PostSourse postSourse;
+    private Repost repost;
+    private Views views;
+
+
+    //getters + setters
+}
